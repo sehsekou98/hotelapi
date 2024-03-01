969a161d516cc84ff3023417a8b3b8d09c3cc915
Sekou S Seh <sehsekou98@gmail.com>
2024-03-01 02:26:32 +0100

    bookroom response

--- src/main/java/org/sekou/lisamhotel/response/BookedRoomResponse.java
+++ src/main/java/org/sekou/lisamhotel/response/BookedRoomResponse.java
@@ -1,4 +1,31 @@
 package org.sekou.lisamhotel.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoomResponse {
+    private Long id;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private String guestFullName;
+    private String guestEmail;
+    private  int numOfGuest;
+    private int numOfChildren;
+    private  int totalNumOfGuest;
+    private String bookingConfirmationCode;
+    private RoomResponse room;
+
+
+    public BookedRoomResponse(Long id, LocalDate checkInDate, LocalDate checkOutDate, String bookingConfirmationCode) {
+        this.id = id;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
