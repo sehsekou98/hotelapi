c0be3e713bbb590a5a7e6b8a66ad4b43e486adce
Sekou S Seh <sehsekou98@gmail.com>
2024-03-01 02:03:18 +0100

    Room service

--- src/main/java/org/sekou/lisamhotel/model/BookedRoom.java
+++ src/main/java/org/sekou/lisamhotel/model/BookedRoom.java
@@ -1,4 +1,70 @@
 package org.sekou.lisamhotel.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    @Column(name = "check_In")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_In")
+    private LocalDate checkOutDate;
+
+    @Column(name = "FullName")
+    private String guestFullName;
+
+    @Column(name = "Email")
+    private String guestEmail;
+
+    @Column(name = "Adults")
+    private int NumOfAdults;
+
+    @Column(name = "Children")
+    private int NumOfChildren;
+
+    @Column(name = "Total_Guest")
+    private int totalNumberOfGuest;
+
+    @Column(name = "Comfirmation_code")
+    private String bookingConfirmationCode;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+
+    public void calculateTotalNumberOfGuest(){
+        this.totalNumberOfGuest = this.NumOfAdults + this.NumOfChildren;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        NumOfAdults = numOfAdults;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        NumOfChildren = numOfChildren;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
