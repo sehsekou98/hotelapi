6d54017c4d01618fff08fac94ebe201231e4db29
Sekou S Seh <sehsekou98@gmail.com>
2024-03-17 22:04:52 +0000

    update

---
+++ src/main/java/org/sekou/lisamhotel/config/JwtAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package org.sekou.lisamhotel.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+          @NonNull HttpServletRequest request,
+          @NonNull  HttpServletResponse response,
+          @NonNull  FilterChain filterChain)
+            throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/org/sekou/lisamhotel/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.sekou.lisamhotel.repository;
+
+
+import org.sekou.lisamhotel.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+}
