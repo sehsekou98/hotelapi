706ed8c3b41e5ca4efcc74c9513a97d5fcb8f0e1
Sekou S Seh <sehsekou98@gmail.com>
2024-03-17 20:56:48 +0000

    update

--- src/main/java/org/sekou/lisamhotel/model/User.java
+++ src/main/java/org/sekou/lisamhotel/model/User.java
@@ -1,34 +1,27 @@
 package org.sekou.lisamhotel.model;
 
-
-import jakarta.persistence.*;
-import lombok.Getter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import java.util.Collection;
-import java.util.HashSet;
-
-@Entity
-@Getter
-@Setter
+@Data
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "_user")
+
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue
+    private Integer id;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
-    @ManyToMany(fetch = FetchType.EAGER,
-            cascade = {CascadeType.PERSIST,
-                    CascadeType.MERGE, CascadeType.DETACH})
-    @JoinTable(name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
-    private Collection<Role> roles = new HashSet<>();
-
-
-
 }
